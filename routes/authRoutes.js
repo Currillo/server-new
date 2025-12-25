a897f478f479d31682da94c69b4482fc24dc4f0d
Currillo <marcialmorral@gmail.com>
2025-12-25 19:54:13 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
+
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../utils/auth');
-const { registerUser, loginUser, getUserProfile, updateDeck } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, updateDeck, updateProfile } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
 router.put('/deck', protect, updateDeck);
+router.put('/updateProfile', protect, updateProfile); // New Route
 
 module.exports = router;
